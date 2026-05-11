9f4059f581e024f0ee5807001c77e971775f2aee
Sara Romero <sara.romerop256@gmail.com>
2026-05-11 09:35:48 +0200

    feat: control de favoritos

--- src/app/favoritos/favoritos.ts
+++ src/app/favoritos/favoritos.ts
@@ -1,9 +1,111 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { Pelicula } from '../interfaces/pelicula';
+import { Peliculas } from '../servicios/peliculas';
+
+import { Estrella } from '../componentes/estrella/estrella';
 
 @Component({
   selector: 'app-favoritos',
-  imports: [],
+
+  imports: [
+    CommonModule,
+    Estrella
+  ],
+
   templateUrl: './favoritos.html',
+
   styleUrl: './favoritos.css',
 })
-export class Favoritos {}
+export class Favoritos implements OnInit {
+
+  favoritos: Pelicula[] = [];
+
+  ordenActual: string = 'alfabeto';
+
+  constructor(
+    private peliculasService: Peliculas
+  ) {}
+
+  ngOnInit(): void {
+
+    this.peliculasService.favoritos$
+      .subscribe((peliculas: Pelicula[]) => {
+
+        this.favoritos = peliculas;
+
+        this.ordenarFavoritos();
+      });
+  }
+
+  ordenarFavoritos(): void {
+
+    switch(this.ordenActual) {
+
+      // ======================
+      // ALFABÉTICO
+      // ======================
+      case 'alfabeto':
+
+        this.favoritos.sort((a, b) =>
+          a.title.localeCompare(b.title)
+        );
+
+      break;
+
+      // ======================
+      // AÑO ASC
+      // ======================
+      case 'yearAsc':
+
+        this.favoritos.sort((a, b) =>
+          a.year - b.year
+        );
+
+      break;
+
+      // ======================
+      // AÑO DESC
+      // ======================
+      case 'yearDesc':
+
+        this.favoritos.sort((a, b) =>
+          b.year - a.year
+        );
+
+      break;
+
+      // ======================
+      // ESTRELLAS ASC
+      // ======================
+      case 'starsAsc':
+
+        this.favoritos.sort((a, b) =>
+          a.stars - b.stars
+        );
+
+      break;
+
+      // ======================
+      // ESTRELLAS DESC
+      // ======================
+      case 'starsDesc':
+
+        this.favoritos.sort((a, b) =>
+          b.stars - a.stars
+        );
+
+      break;
+    }
+  }
+
+  cambiarOrden(event: Event): void {
+
+    const select = event.target as HTMLSelectElement;
+
+    this.ordenActual = select.value;
+
+    this.ordenarFavoritos();
+  }
+}
